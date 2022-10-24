1ff5dab7d3f846c606460e56a0d0bb993ae3e8dd
Uco Mesdag <uco@mesd.ag>
2022-10-24 16:44:50 +0200

    Fixed a breaking change in the upstream container image that made MySQL the default database.

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 # Add FQDN to /etc/hosts for sendmail to work
 echo "$(cat /etc/hosts | tail -n1 | awk '{print $1" "$2" "$2".localdomain"}')" >> /etc/hosts
 
+# Use the sqlite3 driver
+export database__client=sqlite3
+export database__connection__filename=/var/lib/ghost/content/data/ghost.db
+
 /usr/local/bin/docker-entrypoint.sh node current/index.js
